75d52de607256898ce97e7ae7a709b009e4fd8fb
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-05-31 09:37:01 -0300

    upp

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 00:31:55 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/05/31 09:31:56 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/05/31 09:36:38 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*pont;
-	size_t	i;
 
-	i = 0;
 	pont = malloc(nmemb * size);
 	if (!pont)
 		return (NULL);
-	ft_bzero(pont, nmemb * size)
+	ft_bzero(pont, nmemb * size);
 	return (pont);
 }
