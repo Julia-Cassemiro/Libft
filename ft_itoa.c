334a58e2a09b66f80c8df52d2e80a993bb535120
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-05-31 12:32:34 -0300

    upp

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,46 +6,36 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/28 17:22:48 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/05/31 11:21:20 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/05/31 12:29:31 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcpy(char *dest, const char *src)
-{
-	unsigned int	index;
-
-	index = 0;
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-	dest[index] = '\0';
-	return (dest);
-}
-
 char	*ft_itoa(int n)
 {
-	char	*str;
+	int			sign;
+	int			size;
+	char		*conv;
+	long int	nn;
 
-	if (!(str == (char *)malloc(sizeof(char) * 2)))
+	sign = n < 0;
+	nn = n;
+	if (sign > 0)
+		nn = -nn;
+	size = 1;
+	while (n / 10 != 0 && size++)
+		n /= 10;
+	conv = malloc(sizeof(char) * (size + sign + 1));
+	if (!conv)
 		return (NULL);
-	if (n == -2147483648)
-		return (ft_strcpy(str, "-2147483648"));
-	if (n < 0)
+	conv[size + sign] = '\0';
+	while (size-- != 0)
 	{
-		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
+		conv[size + sign] = nn % 10 + '0';
+		nn /= 10;
 	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
-	{
-		str[0] = n + '0';
-		str[1] = '\0';
-	}
-	return (str);
+	if (sign > 0)
+		conv[0] = '-';
+	return (conv);
 }
