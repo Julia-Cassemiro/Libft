6569b0b0152da1afc1de79462b357448e5df1fb0
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-06-02 16:55:58 -0300

    upp memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -20,22 +20,17 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	s_dst = (unsigned char *)dst;
 	s_src = (unsigned char *)src;
+	if (dst == src)
+		return (dst);
+	if (dst < src)
+		ft_memcpy(s_dst, s_src, len);
 	if (s_dst > s_src)
 	{
-		i = (int)len - 1;
+		i = ((int)len - 1);
 		while (i >= 0)
 		{
 			*(char *)(s_dst + i) = *(char *)(s_src + i);
 			i--;
-		}
-	}
-	else
-	{
-		i = 0;
-		while (i < (int)len)
-		{
-			*(char *)(s_dst + i) = *(char *)(s_src + i);
-			i++;
 		}
 	}
 	return (s_dst);
